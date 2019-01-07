5fee9e295311f9ee17982b5d45a5fa58a1782811
pratapshubham <35607443+pratapshubham@users.noreply.github.com>
2019-01-07 18:39:25 +0530

    Add files via upload

---
+++ H.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class H
+{
+
+  
+   public static void main(String []args)
+{
+
+ int n;
+Scanner sc = new Scanner(System.in);
+n = sc.nextInt();
+if(n > 0)
+{
+System.out.println("Positive");
+}     
+else if(n < 0)
+{
+System.out.println("Negative");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
