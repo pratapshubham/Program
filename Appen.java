848cb909e0ddff4d655069b2f98d60846fc29930
pratapshubham <35607443+pratapshubham@users.noreply.github.com>
2019-01-08 16:31:55 +0530

    Add files via upload

---
+++ Appen.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Appen
+{
+
+  
+   public static void main(String[] args)
+	 {
+        
+        String a;
+        
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextLine();
+        StringBuffer sb = new StringBuffer(a);
+        //sb.append(".");
+        System.out.println(sb.append("."));
+        
+    }
+}
